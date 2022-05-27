797c2ece54ee707e216a4120e5a5781219a3e365
Hammed Shittu <92582319+Amirkhan02@users.noreply.github.com>
2022-05-27 14:39:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, track, score):
-                 self.name = name
-                 self.age = age
-                 self.track = track
-                 self.score = score
-                 print("my name is", name, "and I am age", age, "my track is", track, "my current score is", score) 
-        def speak(self):
-        pass
-    d = student("Tunde", 24, "Python", 12.5)
-    #methods
-    class student(*args):
-    def change_name(self, args):
-        
-        self.args = Bob.change_name()
-        self.args = Bob.change_age()
-        self.args = Bob.change_track()
-        self.args = Bob.get_score()
-    def get_input(self args):
-        self_args = self.args("change name", "change age", "add track" "get score")
-         print(self.args)
-         get_input()
+ # Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+import string
+def read_file_content(filename):
+    # [assignment] Add your code here
+    with open(filename) as f:
+         data=f.read().split()
+    return data
 
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    d = {}
+    for line in text:
+        line = line.strip()
+        line = line.lower()
+        line = line.translate(line.maketrans("","", string.punctuation))
+        words = line.split(" ")
+        for word in words:
+            if word in d:
+                d[word] = d[word] + 1
+                else:
+                    d[word] = 1
 
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+                    for key in list(d.keys()):
+                        print(key, ":", d[key])
+                        print(d)
+    
